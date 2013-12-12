0e5d1d6f10e847948f59475c3c68c4f4b7b8ce3e
Norman Jäckel <openslides-csv-export@normanjaeckel.de>
2013-12-12 20:05:38 +0100

    Use entry point.

--- openslides_csv_export/__init__.py
+++ openslides_csv_export/__init__.py
@@ -3,11 +3,13 @@
 
 from inspect import stack
 
-lines = stack()[-1][4]
-
-if not (lines and 'DaM2jee2el7ziech5Shi8bin5fohjee6aimuJae7' in lines[0]):
+for frame in stack():
+    lines = frame[4]
+    if lines and 'DaM2jee2el7ziech5Shi8bin5fohjee6aimuJae7' in lines[0]:
+        break
+else:
     from .urls import urlpatterns  # noqa
 
-__description__ = 'This plugin for OpenSlides provides a csv export of the lists of speakers.'
 __verbose_name__ = 'CSV Export Plugin for OpenSlides'
+__description__ = 'This plugin for OpenSlides provides a csv export of the lists of speakers.'
 __version__ = '1.1.0-dev'
--- setup.py
+++ setup.py
@@ -2,26 +2,25 @@
 
 from setuptools import find_packages, setup
 
-# The following commented unique string is used to detect this import.
-from openslides_csv_export import __verbose_name__ as verbose_name, __version__ as version  # DaM2jee2el7ziech5Shi8bin5fohjee6aimuJae7
+module_name = 'openslides_csv_export'
 
+# The following commented unique string is used to detect this import.
+module = __import__(module_name)  # DaM2jee2el7ziech5Shi8bin5fohjee6aimuJae7
 
 with open('README.rst') as readme:
     long_description = readme.read()
 
-
 with open('requirements_production.txt') as requirements_production:
     install_requires = requirements_production.readlines()
 
-
 setup(
     name='openslides-csv-export',
-    version=version,
-    description=verbose_name,
+    version=module.__version__,
+    description=module.__verbose_name__,
     long_description=long_description,
     author='OpenSlides-Team, see AUTHORS',
     author_email='support@openslides.org',
-    url='http://openslides.org',
+    url='http://openslides.org/',
     keywords='OpenSlides',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -34,4 +33,5 @@ setup(
     license='MIT',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    install_requires=install_requires)
+    install_requires=install_requires,
+    entry_points={'openslides_plugins': '%s = %s' % (module.__verbose_name__, module_name)})
