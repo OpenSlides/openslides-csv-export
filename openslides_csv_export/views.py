4ff0a450491b6e65e65aecb4c142cff7fb049bb0
Norman Jäckel <openslides-csv-export@normanjaeckel.de>
2016-02-16 17:19:38 +0100

    Moved to OpenSlides 2.x.

--- openslides_csv_export/views.py
+++ openslides_csv_export/views.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-
 import csv
 
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from openslides.agenda.models import Speaker
-from openslides.utils.views import PermissionMixin, View
+from openslides.utils.views import View
 
 
-class CSVExportView(PermissionMixin, View):
+class CSVExportView(View):
     """
     View to export the lists of speakers of all agenda items as csv.
     """
-    required_permission = 'agenda.can_manage_agenda'
-
     def get(self, request, *args, **kwargs):
+        if not request.user.has_perm('agenda.can_manage'):
+            raise PermissionDenied
         response = HttpResponse()
         response['Content-Disposition'] = 'attachment; filename=list_of_speakers.csv;'
         csv_writer = csv.writer(response)
@@ -27,6 +26,5 @@ class CSVExportView(PermissionMixin, View):
                 end_time = speaker.end_time.strftime('%d.%m.%Y %H:%M:%S')
             except AttributeError:
                 end_time = None
-            csv_writer.writerow([
-                speaker.item.get_title().encode('utf8'), unicode(speaker.person).encode('utf8'), begin_time, end_time])
+            csv_writer.writerow([str(speaker.item), str(speaker.user), begin_time, end_time])
         return response
--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from setuptools import find_packages, setup
 
 package_name = 'openslides-csv-export'
 module_name = 'openslides_csv_export'
 
-# The following commented unique string is used to detect this import.
-module = __import__(module_name)  # EuC5Kee6ohp7aesae3yai3fe3soo2uzoshohk2uj
+module = __import__(module_name)
 
 with open('README.rst') as readme:
     long_description = readme.read()
@@ -16,12 +13,14 @@ with open('requirements.txt') as requirements:
 
 setup(
     name=package_name,
-    version=module.__version__,
-    description=module.__verbose_name__,
-    long_description=long_description,
     author='Authors of %s, see AUTHORS' % module.__verbose_name__,
     author_email='support@openslides.org',
+    description=module.__verbose_name__,
+    license='MIT',
+    long_description=long_description,
     url='http://openslides.org/',
+    version=module.__version__,
+
     keywords='OpenSlides',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -30,8 +29,9 @@ setup(
         'Framework :: Django',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2'],
-    license='MIT',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requires,
