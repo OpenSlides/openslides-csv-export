ef3b4c6f68882785946599160e4f793728c4efba
Norman Jäckel <openslides-csv-export@normanjaeckel.de>
2016-06-14 23:43:45 +0200

    Added small example how to use states and templates.

--- openslides_csv_export/static/js/openslides_csv_export/site.js
+++ openslides_csv_export/static/js/openslides_csv_export/site.js
@@ -9,12 +9,23 @@ angular.module('OpenSlidesApp.openslides_csv_export.site', ['OpenSlidesApp.opens
     'gettext',
     function (mainMenuProvider, gettext) {
         mainMenuProvider.register({
-            'ui_sref': 'csv_export_list_of_speakers',
+            'ui_sref': 'openslides_csv_export_overview',
             'img_class': 'download',
             'title': gettext('CSV Export'),
             'weight': 2000,
             'perm': 'agenda.can_manage',
         });
+    }
+])
+
+.config([
+    '$stateProvider',
+    function ($stateProvider) {
+        $stateProvider
+            .state('openslides_csv_export_overview', {
+                url: '/openslides_csv_export',
+                templateUrl: 'static/templates/openslides_csv_export/overview.html',
+            });
     }
 ]);
 
